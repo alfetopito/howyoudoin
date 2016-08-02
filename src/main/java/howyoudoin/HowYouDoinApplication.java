d41e0abb35f851d564536569258244f899a41b05
Leandro Boscariol <leandroboscariol@gmail.com>
2016-08-02 16:33:03 -0300

    Changed package name to something other than com.example

---
+++ src/main/java/howyoudoin/HowYouDoinApplication.java
@@ -0,0 +1,12 @@
+package howyoudoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowYouDoinApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(HowYouDoinApplication.class, args);
+	}
+}
